29261ae218ebb640adb4553b22e7e0812c5e5fe4
MartaCzapka <01123769@pw.edu.pl>
2021-01-14 12:43:39 +0000

    zad4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -1,8 +1,19 @@
 #include "zad1.hpp"
 
 #include <vector>
-
+using namespace std;
 // tutaj funkcja obliczMake
 double obliczMake(const vector<Tagliatelle>& maka){
-
+double M=0;
+int n=maka.size();
+for (auto n:maka){
+  M+=maka.ileMaki(n);
+  n--;
+}
+  if(M>100)
+  throw  (1);
+  else if(M>50)
+  throw (0.5);
+else
+return M;
 }
